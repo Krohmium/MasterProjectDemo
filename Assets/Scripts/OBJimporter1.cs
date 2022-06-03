85d4d43e02a74395d5a0be83afb271f7ec7c6739
Paul Höfler <paul.hoefler@student.tugraz.at>
2022-06-03 14:15:12 +0200

    initial

--- Assets/Scripts/OBJimporter1.cs
+++ Assets/Scripts/OBJimporter1.cs
@@ -1,106 +1,106 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Dummiesman;
-using UnityEditor;
-using System.IO;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Dummiesman;
+//using UnityEditor;
+//using System.IO;
 
-namespace AsImpL
-{
-    public class OBJimporter1 : MonoBehaviour
-    {
-        //protected ObjectImporter objImporter;
+//namespace AsImpL
+//{
+//    public class OBJimporter1 : MonoBehaviour
+//    {
+//        //protected ObjectImporter objImporter;
 
-        string objPath = string.Empty;
-        string objPath_material = string.Empty;
-        string extension = string.Empty;
+//        string objPath = string.Empty;
+//        string objPath_material = string.Empty;
+//        string extension = string.Empty;
 
-        string error = string.Empty;
-        GameObject loadedObject;
-        MeshCollider tempCollider;
-        List<GameObject> loadedObjects = new List<GameObject>();
-
-
-
-
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            int counter = 0;
-            objPath = "C:\\Users\\Krohm\\Documents\\Uni\\Masterarbeit\\NHMV Models\\temp\\obj\\figure-holding-the-hraschina-meteorite\\Karyatide_Hraschina_Saal4_EDIT_VW_lowres.obj";
-            objPath_material = "C:\\Users\\Krohm\\Documents\\Uni\\Masterarbeit\\NHMV Models\\temp\\obj\\figure-holding-the-hraschina-meteorite\\Karyatide_Hraschina_Saal4_EDIT_VW_lowres.mtl";
-
-            foreach (string directory in System.IO.Directory.GetDirectories("C:\\Users\\Krohm\\Documents\\Uni\\Masterarbeit\\NHMV Models\\temp\\obj\\"))
-            {
-                if (counter > 8)
-                    break;
-
-                foreach (string file in System.IO.Directory.GetFiles(directory))
-                {
-                    extension = file.Split(".")[1];
-
-                    if (extension == "obj")
-                        objPath = file;
-                    if (extension == "mtl")
-                        objPath_material = file;
-                }
-
-                loadedObject = new OBJLoader().Load(objPath, objPath_material);
-
-                //objImporter.ImportModelAsync(objectName, filePath, null, importOptions);
-
-                loadedObject.transform.localScale = new Vector3(-0.01f, -0.01f, -0.01f);
-                loadedObject.transform.position = new Vector3(-40.0f + counter * 10.0f, 8.75f, 20.0f);
-                loadedObject.transform.Rotate(new Vector3(90.0f, 180.0f, 0.0f));
-
-                //loadedObject.GetComponentInChildren<MeshRenderer>().material.shader = Shader.Find("Standard");
-                //loadedObject.GetComponentInChildren<MeshRenderer>().material.SetFloat("_Glossiness", 0.0f);
-
-                GameObject childGameObject;
-                MeshRenderer childGameObjectRenderer;
-                for (int child = 0; child < loadedObject.transform.childCount; child++)
-                {
-                    childGameObject = loadedObject.transform.GetChild(child).gameObject;
-                    childGameObjectRenderer = childGameObject.GetComponent<MeshRenderer>();
-                    childGameObjectRenderer.material.shader = Shader.Find("Standard");
-                    childGameObjectRenderer.material.SetFloat("_Glossiness", 0.0f);
-
-                    tempCollider = childGameObject.AddComponent<MeshCollider>();
-                    tempCollider.convex = true;
-                    childGameObject.AddComponent<Rigidbody>();
-                }
-
-
-                counter++;
-            }
-
-
-            //if (loadedObject != null)
-            //    Destroy(loadedObject);
-            //loadedObject = new OBJLoader().Load(objPath, objPath_material);
-
-
-            //loadedObject.transform.localScale = new Vector3(-0.01f, -0.01f, -0.01f);
-            //loadedObject.transform.position = new Vector3(0.0f, 3.75f, 40.0f);
-            //loadedObject.transform.Rotate(new Vector3(90.0f, 180.0f, 0.0f));
-
-            //loadedObject.GetComponentInChildren<MeshRenderer>().material.shader = Shader.Find("Standard");
-            //loadedObject.GetComponentInChildren<MeshRenderer>().material.SetFloat("_Glossiness", 0.0f);
-
-
-            //Destroy(loadedObject.GetComponent<MeshRenderer>());
-            //loadedObject.GetComponentInChildren<MeshRenderer>().material = "defaultMat"; 
+//        string error = string.Empty;
+//        GameObject loadedObject;
+//        MeshCollider tempCollider;
+//        List<GameObject> loadedObjects = new List<GameObject>();
 
 
 
 
-        }
 
-        // Update is called once per frame
-        void Update()
-        {
+//        // Start is called before the first frame update
+//        void Start()
+//        {
+//            int counter = 0;
+//            objPath = "C:\\Users\\Krohm\\Documents\\Uni\\Masterarbeit\\NHMV Models\\temp\\obj\\figure-holding-the-hraschina-meteorite\\Karyatide_Hraschina_Saal4_EDIT_VW_lowres.obj";
+//            objPath_material = "C:\\Users\\Krohm\\Documents\\Uni\\Masterarbeit\\NHMV Models\\temp\\obj\\figure-holding-the-hraschina-meteorite\\Karyatide_Hraschina_Saal4_EDIT_VW_lowres.mtl";
 
-        }
-    }
-}
+//            foreach (string directory in System.IO.Directory.GetDirectories("C:\\Users\\Krohm\\Documents\\Uni\\Masterarbeit\\NHMV Models\\temp\\obj\\"))
+//            {
+//                if (counter > 8)
+//                    break;
+
+//                foreach (string file in System.IO.Directory.GetFiles(directory))
+//                {
+//                    extension = file.Split(".")[1];
+
+//                    if (extension == "obj")
+//                        objPath = file;
+//                    if (extension == "mtl")
+//                        objPath_material = file;
+//                }
+
+//                loadedObject = new OBJLoader().Load(objPath, objPath_material);
+
+//                //objImporter.ImportModelAsync(objectName, filePath, null, importOptions);
+
+//                loadedObject.transform.localScale = new Vector3(-0.01f, -0.01f, -0.01f);
+//                loadedObject.transform.position = new Vector3(-40.0f + counter * 10.0f, 8.75f, 20.0f);
+//                loadedObject.transform.Rotate(new Vector3(90.0f, 180.0f, 0.0f));
+
+//                //loadedObject.GetComponentInChildren<MeshRenderer>().material.shader = Shader.Find("Standard");
+//                //loadedObject.GetComponentInChildren<MeshRenderer>().material.SetFloat("_Glossiness", 0.0f);
+
+//                GameObject childGameObject;
+//                MeshRenderer childGameObjectRenderer;
+//                for (int child = 0; child < loadedObject.transform.childCount; child++)
+//                {
+//                    childGameObject = loadedObject.transform.GetChild(child).gameObject;
+//                    childGameObjectRenderer = childGameObject.GetComponent<MeshRenderer>();
+//                    childGameObjectRenderer.material.shader = Shader.Find("Standard");
+//                    childGameObjectRenderer.material.SetFloat("_Glossiness", 0.0f);
+
+//                    tempCollider = childGameObject.AddComponent<MeshCollider>();
+//                    tempCollider.convex = true;
+//                    childGameObject.AddComponent<Rigidbody>();
+//                }
+
+
+//                counter++;
+//            }
+
+
+//            //if (loadedObject != null)
+//            //    Destroy(loadedObject);
+//            //loadedObject = new OBJLoader().Load(objPath, objPath_material);
+
+
+//            //loadedObject.transform.localScale = new Vector3(-0.01f, -0.01f, -0.01f);
+//            //loadedObject.transform.position = new Vector3(0.0f, 3.75f, 40.0f);
+//            //loadedObject.transform.Rotate(new Vector3(90.0f, 180.0f, 0.0f));
+
+//            //loadedObject.GetComponentInChildren<MeshRenderer>().material.shader = Shader.Find("Standard");
+//            //loadedObject.GetComponentInChildren<MeshRenderer>().material.SetFloat("_Glossiness", 0.0f);
+
+
+//            //Destroy(loadedObject.GetComponent<MeshRenderer>());
+//            //loadedObject.GetComponentInChildren<MeshRenderer>().material = "defaultMat"; 
+
+
+
+
+//        }
+
+//        // Update is called once per frame
+//        void Update()
+//        {
+
+//        }
+//    }
+//}
